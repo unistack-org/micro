2c136b005edd59d621534941ffd8b8bd7ebdcc14
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-08-28 11:55:18 +0300

    resurrect default for auth
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- auth/auth.go
+++ auth/auth.go
@@ -17,6 +17,7 @@ const (
 )
 
 var (
+	DefaultAuth Auth
 	// ErrInvalidToken is when the token provided is not valid
 	ErrInvalidToken = errors.New("invalid token provided")
 	// ErrForbidden is when a user does not have the necessary scope to access a resource
