6e43ae7190765b1208abe20d4ad8916006dcff87
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-10-12 12:09:09 +0300

    add client publish option func
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- client/context.go
+++ client/context.go
@@ -14,3 +14,13 @@ func FromContext(ctx context.Context) (Client, bool) {
 func NewContext(ctx context.Context, c Client) context.Context {
 	return context.WithValue(ctx, clientKey{}, c)
 }
+
+// SetPublishOption returns a function to setup a context with given value
+func SetPublishOption(k, v interface{}) PublishOption {
+	return func(o *PublishOptions) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, k, v)
+	}
+}
