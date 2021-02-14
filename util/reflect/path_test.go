67748a2132ede5cbfd1b4f8a63a3d7e57285d7a9
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-02-14 23:33:01 +0300

    util/reflect: import own path based interface lookup
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ util/reflect/path_test.go
@@ -0,0 +1,34 @@
+package reflect
+
+import (
+	"testing"
+)
+
+func TestPath(t *testing.T) {
+
+	type Nested2 struct {
+		Name string
+	}
+	type Nested1 struct {
+		Nested2 Nested2
+	}
+	type Config struct {
+		Nested1 Nested1
+	}
+
+	cfg := &Config{
+		Nested1: Nested1{
+			Nested2: Nested2{
+				Name: "NAME",
+			},
+		},
+	}
+
+	v, err := Lookup(cfg, "$.Nested1.Nested2.Name")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v.String() != "NAME" {
+		t.Fatalf("lookup returns invalid value: %v", v)
+	}
+}
