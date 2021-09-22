875f66d36e59bf5b3de97396c49d173aecb025f4
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-09-22 16:59:52 +0300

    codec: implement proto v1 message for Frame
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- codec/frame.go
+++ codec/frame.go
@@ -4,3 +4,31 @@ package codec
 type Frame struct {
 	Data []byte
 }
+
+func (m *Frame) MarshalJSON() ([]byte, error) {
+	return m.Data, nil
+}
+
+func (m *Frame) UnmarshalJSON(data []byte) error {
+	m.Data = data
+	return nil
+}
+
+func (m *Frame) ProtoMessage() {}
+
+func (m *Frame) Reset() {
+	*m = Frame{}
+}
+
+func (m *Frame) String() string {
+	return string(m.Data)
+}
+
+func (m *Frame) Marshal() ([]byte, error) {
+	return m.Data, nil
+}
+
+func (m *Frame) Unmarshal(data []byte) error {
+	m.Data = data
+	return nil
+}
