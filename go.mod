3087ba1d739f5a8e9110e0cbf4a85cea467693b5
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-10-23 23:45:53 +0300

    regen
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/imdario/mergo v0.3.12
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/silas/dag v0.0.0-20210626123444-3804bac2d6d4
-	github.com/unistack-org/micro-proto v0.0.9
+	go.unistack.org/micro-proto/v3 v3.1.0
 	golang.org/x/net v0.0.0-20210928044308-7d9f5e0b762b
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- meter/generate.go
+++ meter/generate.go
@@ -1,12 +1,12 @@
 package meter
 
-//go:generate sh -c "protoc -I./handler -I../ -I$(go list -f '{{ .Dir }}' -m github.com/unistack-org/micro-proto) --go-micro_out='components=micro|http|server',standalone=false,debug=true,paths=source_relative:./handler handler/handler.proto"
+//go:generate sh -c "protoc -I./handler -I../ -I$(go list -f '{{ .Dir }}' -m go.unistack.org/micro-proto/v3) --go-micro_out='components=micro|http|server',standalone=false,debug=true,paths=source_relative:./handler handler/handler.proto"
 
 import (
 
 	// import required packages
-	_ "github.com/unistack-org/micro-proto/api"
+	_ "go.unistack.org/micro-proto/v3/api"
 
 	// import required packages
-	_ "github.com/unistack-org/micro-proto/openapiv2"
+	_ "go.unistack.org/micro-proto/v3/openapiv3"
 )
