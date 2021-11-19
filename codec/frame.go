d29363b78d14e29f13a7ac2fd695fa38bde41255
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-11-19 09:22:13 +0300

    codec: add NewFrame helper
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- codec/frame.go
+++ codec/frame.go
@@ -5,29 +5,40 @@ type Frame struct {
 	Data []byte
 }
 
+// NewFrame returns new frame with data
+func NewFrame(data []byte) *Frame {
+	return &Frame{Data: data}
+}
+
+// MarshalJSON returns frame data
 func (m *Frame) MarshalJSON() ([]byte, error) {
-	return m.Data, nil
+	return m.Marshal()
 }
 
+// UnmarshalJSON set frame data
 func (m *Frame) UnmarshalJSON(data []byte) error {
-	m.Data = data
-	return nil
+	return m.Unmarshal(data)
 }
 
+// ProtoMessage noop func
 func (m *Frame) ProtoMessage() {}
 
+// Reset resets frame
 func (m *Frame) Reset() {
 	*m = Frame{}
 }
 
+// String returns frame as string
 func (m *Frame) String() string {
 	return string(m.Data)
 }
 
+// Marshal returns frame data
 func (m *Frame) Marshal() ([]byte, error) {
 	return m.Data, nil
 }
 
+// Unmarshal set frame data
 func (m *Frame) Unmarshal(data []byte) error {
 	m.Data = data
 	return nil
