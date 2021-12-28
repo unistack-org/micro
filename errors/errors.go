8ce469a09ea47f7a39ae116e7e0c3a245e52d617
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-12-28 09:18:52 +0300

    tracer: fixes
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- errors/errors.go
+++ errors/errors.go
@@ -53,6 +53,22 @@ func (e *Error) Error() string {
 	return string(b)
 }
 
+/*
+// Generator struct holds id of error
+type Generator struct {
+	id string
+}
+
+// Generator can emit new error with static id
+func NewGenerator(id string) *Generator {
+	return &Generator{id: id}
+}
+
+func (g *Generator) BadRequest(format string, args ...interface{}) error {
+	return BadRequest(g.id, format, args...)
+}
+*/
+
 // New generates a custom error
 func New(id, detail string, code int32) error {
 	return &Error{
@@ -75,121 +91,121 @@ func Parse(err string) *Error {
 }
 
 // BadRequest generates a 400 error.
-func BadRequest(id, format string, a ...interface{}) error {
+func BadRequest(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   400,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(400),
 	}
 }
 
 // Unauthorized generates a 401 error.
-func Unauthorized(id, format string, a ...interface{}) error {
+func Unauthorized(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   401,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(401),
 	}
 }
 
 // Forbidden generates a 403 error.
-func Forbidden(id, format string, a ...interface{}) error {
+func Forbidden(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   403,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(403),
 	}
 }
 
 // NotFound generates a 404 error.
-func NotFound(id, format string, a ...interface{}) error {
+func NotFound(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   404,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(404),
 	}
 }
 
 // MethodNotAllowed generates a 405 error.
-func MethodNotAllowed(id, format string, a ...interface{}) error {
+func MethodNotAllowed(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   405,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(405),
 	}
 }
 
 // Timeout generates a 408 error.
-func Timeout(id, format string, a ...interface{}) error {
+func Timeout(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   408,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(408),
 	}
 }
 
 // Conflict generates a 409 error.
-func Conflict(id, format string, a ...interface{}) error {
+func Conflict(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   409,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(409),
 	}
 }
 
 // InternalServerError generates a 500 error.
-func InternalServerError(id, format string, a ...interface{}) error {
+func InternalServerError(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   500,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(500),
 	}
 }
 
 // NotImplemented generates a 501 error
-func NotImplemented(id, format string, a ...interface{}) error {
+func NotImplemented(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   501,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(501),
 	}
 }
 
 // BadGateway generates a 502 error
-func BadGateway(id, format string, a ...interface{}) error {
+func BadGateway(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   502,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(502),
 	}
 }
 
 // ServiceUnavailable generates a 503 error
-func ServiceUnavailable(id, format string, a ...interface{}) error {
+func ServiceUnavailable(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   503,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(503),
 	}
 }
 
 // GatewayTimeout generates a 504 error
-func GatewayTimeout(id, format string, a ...interface{}) error {
+func GatewayTimeout(id, format string, args ...interface{}) error {
 	return &Error{
 		ID:     id,
 		Code:   504,
-		Detail: fmt.Sprintf(format, a...),
+		Detail: fmt.Sprintf(format, args...),
 		Status: http.StatusText(504),
 	}
 }
