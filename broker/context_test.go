8ff312e71da6b052081d49f2438d4db24b411763
Vasiliy Tolstov <v.tolstov@unistack.org>
2022-05-03 15:51:08 +0300

    broker: improve coverage
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- broker/context_test.go
+++ broker/context_test.go
@@ -7,16 +7,31 @@ import (
 
 func TestFromContext(t *testing.T) {
 	ctx := context.WithValue(context.TODO(), brokerKey{}, NewBroker())
-
 	c, ok := FromContext(ctx)
 	if c == nil || !ok {
 		t.Fatal("FromContext not works")
 	}
 }
 
+func TestFromNilContext(t *testing.T) {
+	// nolint: staticcheck
+	c, ok := FromContext(nil)
+	if ok || c != nil {
+		t.Fatal("FromContext not works")
+	}
+}
+
 func TestNewContext(t *testing.T) {
 	ctx := NewContext(context.TODO(), NewBroker())
+	c, ok := FromContext(ctx)
+	if c == nil || !ok {
+		t.Fatal("NewContext not works")
+	}
+}
 
+func TestNewNilContext(t *testing.T) {
+	// nolint: staticcheck
+	ctx := NewContext(nil, NewBroker())
 	c, ok := FromContext(ctx)
 	if c == nil || !ok {
 		t.Fatal("NewContext not works")
