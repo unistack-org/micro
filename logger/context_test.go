4ab7f19ef05707ee320129e2fc70083e731ad19d
Vasiliy Tolstov <v.tolstov@unistack.org>
2022-05-03 16:05:36 +0300

    logger: improve coverage
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- logger/context_test.go
+++ logger/context_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
+func TestFromNilContext(t *testing.T) {
+	// nolint: staticcheck
+	c, ok := FromContext(nil)
+	if ok || c != nil {
+		t.Fatal("FromContext not works")
+	}
+}
+
+func TestNewNilContext(t *testing.T) {
+	// nolint: staticcheck
+	ctx := NewContext(nil, NewLogger())
+
+	c, ok := FromContext(ctx)
+	if c == nil || !ok {
+		t.Fatal("NewContext not works")
+	}
+}
+
 func TestFromContext(t *testing.T) {
 	ctx := context.WithValue(context.TODO(), loggerKey{}, NewLogger())
 
