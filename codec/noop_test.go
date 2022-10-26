d659db69ff5d9d57b494fdb28e78c1ee77f7be99
Vasiliy Tolstov <v.tolstov@unistack.org>
2022-10-26 08:26:10 +0300

    codec: add []byte support to noop codec
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- codec/noop_test.go
+++ codec/noop_test.go
@@ -5,9 +5,23 @@ import (
 	"testing"
 )
 
-func TestNoopBytes(t *testing.T) {
+func TestNoopBytesPtr(t *testing.T) {
 	req := []byte("test req")
 	rsp := make([]byte, len(req))
+
+	nc := NewCodec()
+	if err := nc.Unmarshal(req, &rsp); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(req, rsp) {
+		t.Fatalf("req not eq rsp: %s != %s", req, rsp)
+	}
+}
+
+func TestNoopBytes(t *testing.T) {
+	req := []byte("test req")
+	var rsp []byte
 
 	nc := NewCodec()
 	if err := nc.Unmarshal(req, &rsp); err != nil {
