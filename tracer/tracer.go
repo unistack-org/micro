036c61213740e3ffd7b902510cd3f4404ac3ace5
Vasiliy Tolstov <v.tolstov@unistack.org>
2023-09-01 14:55:50 +0300

    tracer: tweaks for span tags and naming
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- tracer/tracer.go
+++ tracer/tracer.go
@@ -3,6 +3,8 @@ package tracer // import "go.unistack.org/micro/v3/tracer"
 
 import (
 	"context"
+	"fmt"
+	"sort"
 )
 
 // DefaultTracer is the global default tracer
@@ -41,4 +43,38 @@ type Span interface {
 	AddLabels(labels ...interface{})
 	// Kind returns span kind
 	Kind() SpanKind
+}
+
+// sort labels alphabeticaly by label name
+type byKey []interface{}
+
+func (k byKey) Len() int           { return len(k) / 2 }
+func (k byKey) Less(i, j int) bool { return fmt.Sprintf("%s", k[i*2]) < fmt.Sprintf("%s", k[j*2]) }
+func (k byKey) Swap(i, j int) {
+	k[i*2], k[j*2] = k[j*2], k[i*2]
+	k[i*2+1], k[j*2+1] = k[j*2+1], k[i*2+1]
+}
+
+func UniqLabels(labels []interface{}) []interface{} {
+	if len(labels)%2 == 1 {
+		labels = labels[:len(labels)-1]
+	}
+
+	if len(labels) > 2 {
+		sort.Sort(byKey(labels))
+
+		idx := 0
+		for {
+			if labels[idx] == labels[idx+2] {
+				copy(labels[idx:], labels[idx+2:])
+				labels = labels[:len(labels)-2]
+			} else {
+				idx += 2
+			}
+			if idx+2 >= len(labels) {
+				break
+			}
+		}
+	}
+	return labels
 }
