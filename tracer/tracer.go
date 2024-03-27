f83a29eb6765410f4e9ced854ae13b06261b45c6
Vasiliy Tolstov <v.tolstov@unistack.org>
2024-03-27 11:24:28 +0300

    move sort.Uniq to dedicated package
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- tracer/tracer.go
+++ tracer/tracer.go
@@ -3,8 +3,6 @@ package tracer // import "go.unistack.org/micro/v3/tracer"
 
 import (
 	"context"
-	"fmt"
-	"sort"
 
 	"go.unistack.org/micro/v3/logger"
 )
@@ -69,38 +67,4 @@ type Span interface {
 	TraceID() string
 	// SpanID returns span id
 	SpanID() string
-}
-
-// sort labels alphabeticaly by label name
-type byKey []interface{}
-
-func (k byKey) Len() int           { return len(k) / 2 }
-func (k byKey) Less(i, j int) bool { return fmt.Sprintf("%s", k[i*2]) < fmt.Sprintf("%s", k[j*2]) }
-func (k byKey) Swap(i, j int) {
-	k[i*2], k[j*2] = k[j*2], k[i*2]
-	k[i*2+1], k[j*2+1] = k[j*2+1], k[i*2+1]
-}
-
-func UniqLabels(labels []interface{}) []interface{} {
-	if len(labels)%2 == 1 {
-		labels = labels[:len(labels)-1]
-	}
-
-	if len(labels) > 2 {
-		sort.Sort(byKey(labels))
-
-		idx := 0
-		for {
-			if labels[idx] == labels[idx+2] {
-				copy(labels[idx:], labels[idx+2:])
-				labels = labels[:len(labels)-2]
-			} else {
-				idx += 2
-			}
-			if idx+2 >= len(labels) {
-				break
-			}
-		}
-	}
-	return labels
 }
