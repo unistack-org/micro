c9061860113fde5f1d3cf8b34c256c98ac7881af
Vasiliy Tolstov <v.tolstov@unistack.org>
2024-04-06 22:03:56 +0300

    add semconv package
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ semconv/client.go
@@ -0,0 +1,12 @@
+package semconv
+
+var (
+	// ClientRequestDurationSeconds specifies meter metric name
+	ClientRequestDurationSeconds = "client_request_duration_seconds"
+	// ClientRequestLatencyMicroseconds specifies meter metric name
+	ClientRequestLatencyMicroseconds = "client_request_latency_microseconds"
+	// ClientRequestTotal specifies meter metric name
+	ClientRequestTotal = "client_request_total"
+	// ClientRequestInflight specifies meter metric name
+	ClientRequestInflight = "client_request_inflight"
+)
