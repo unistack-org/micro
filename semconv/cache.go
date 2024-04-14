b8928d3da978b1bce1d82c451d0be15629344a68
Vasiliy Tolstov <v.tolstov@unistack.org>
2024-04-14 16:42:36 +0300

    semconv: add cache metric names
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ semconv/cache.go
@@ -0,0 +1,12 @@
+package semconv
+
+var (
+	// CacheRequestDurationSeconds specifies meter metric name
+	CacheRequestDurationSeconds = "cache_request_duration_seconds"
+	// ClientRequestLatencyMicroseconds specifies meter metric name
+	CacheRequestLatencyMicroseconds = "cache_request_latency_microseconds"
+	// CacheRequestTotal specifies meter metric name
+	CacheRequestTotal = "cache_request_total"
+	// CacheRequestInflight specifies meter metric name
+	CacheRequestInflight = "cache_request_inflight"
+)
