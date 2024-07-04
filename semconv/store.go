00b3ceb468cb6f7ad95ec8939f38cc18affac8e3
Vasiliy Tolstov <v.tolstov@unistack.org>
2024-07-04 14:56:48 +0300

    smeconv: fix naming
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ semconv/store.go
@@ -0,0 +1,12 @@
+package semconv
+
+var (
+	// StoreRequestDurationSeconds specifies meter metric name
+	StoreRequestDurationSeconds = "store_request_duration_seconds"
+	// ClientRequestLatencyMicroseconds specifies meter metric name
+	StoreRequestLatencyMicroseconds = "store_request_latency_microseconds"
+	// StoreRequestTotal specifies meter metric name
+	StoreRequestTotal = "store_request_total"
+	// StoreRequestInflight specifies meter metric name
+	StoreRequestInflight = "store_request_inflight"
+)
