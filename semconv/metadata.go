0b190997b157da4b59278e9667fe732be2682060
Vasiliy Tolstov <v.tolstov@unistack.org>
2024-12-19 16:03:23 +0300

    metadata: add Copy method, fix old methods
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ semconv/metadata.go
@@ -0,0 +1,18 @@
+package semconv
+
+var (
+	// HeaderTopic is the header name that contains topic name
+	HeaderTopic = "Micro-Topic"
+	// HeaderContentType specifies content type of message
+	HeaderContentType = "Content-Type"
+	// HeaderEndpoint specifies endpoint in service
+	HeaderEndpoint = "Micro-Endpoint"
+	// HeaderService specifies service
+	HeaderService = "Micro-Service"
+	// HeaderTimeout specifies timeout of operation
+	HeaderTimeout = "Micro-Timeout"
+	// HeaderAuthorization specifies Authorization header
+	HeaderAuthorization = "Authorization"
+	// HeaderXRequestID specifies request id
+	HeaderXRequestID = "X-Request-Id"
+)
