3ec53de577682d0f3dc5ea3b189c50831579ebc2
pugnack <pugnack@gmail.com>
2025-06-14 19:31:21 +0500

    add tests for Write() method

--- util/buffer/seeker_buffer.go
+++ util/buffer/seeker_buffer.go
@@ -45,8 +45,7 @@ func (b *SeekerBuffer) Read(p []byte) (int, error) {
 	return n, nil
 }
 
-// Write appends the contents of p to the end of the buffer.
-// It does not affect the read position.
+// Write appends the contents of p to the end of the buffer. It does not affect the read position.
 func (b *SeekerBuffer) Write(p []byte) (int, error) {
 	if len(p) == 0 {
 		return 0, nil
